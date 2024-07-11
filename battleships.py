c8052499d4a50a83e371279b4829e75179472144
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2024-07-11 13:10:50 +0100

    Updated battleships.py
    Added the Functions Random row and Random Column also started adding comments to the variables

--- battleships.py
+++ battleships.py
@@ -1,25 +1,26 @@
 from random import randint
 import copy
 
-OCEAN = "O"
-FIRE = "X"
-HIT = "*"
-SIZE = 10
-SHIPS = [5, 4, 3, 3, 2]
-SEA = []
+#Setting up the Constants and variables
+OCEAN = "O" #icon for the ocean spaces
+FIRE = "X" # icon for a miss
+HIT = "*" #icon for a hit
+SIZE = 10 #size of the board
+SHIPS = [5, 4, 3, 3, 2] # size of each of the ships
+SEA = [] #empty list for the sea
 
-orientation = -1
-total_hits = []
-miss = 1
+orientation = -1 # stores the ships hit orientation.
+total_hits = [] #stores the ship number everytime the bot hits a ship
+miss = 1 #stores whether the last ai shot was a miss
 
-player_ship_lives = 17
+player_ship_lives = 17 #the amount of lives for the player equal to the ships
 player_radar = []
 player_board = []
 
-ai_ship_lives = 17
+ai_ship_lives = 17 #the Ai lives equal to the ship parts
 ai_radar = []
 ai_board = []
-ship_position = []
+ship_position = [] 
 ship_length = []
 
 for x in range(SIZE):
@@ -31,4 +32,17 @@ def print_board():
     i = 0
     for row in range(SIZE):
         print(i, " ".join(player_radar[row]), "||", " ".join(player_board[row]))
+
+def random_row(is_vertical, size):
+    if is_vertical:
+        return randint(0, SIZE - size)
+    else:
+        return randint(0, SIZE - 1)
+
+def random_col(is_vertical, size):
+    if is_vertical:
+        return randint(0, SIZE - size)
+    else:
+        return randint(0, SIZE - 1)
+    
         
